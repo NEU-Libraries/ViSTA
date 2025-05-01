7948e8eb01a16385d132601dcba7c14badd76718
Anh Nguyen <nguyen.anh23@FACG3KWXX14L7.tld>
2025-05-01 11:55:42 -0400

    add print statements for debugging

--- sys_main_gemini.py
+++ sys_main_gemini.py
@@ -162,6 +162,7 @@ def main():
     ViSTA_logger = Logger(LOG_DIR)
     
     log_file_path = ViSTA_logger.generate_log(f"{image_batch_name}_gemini_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_log.csv") 
+    print(log_file_path)
     # log_file_path = ViSTA_logger.generate_log(f'{output_csv}_log') #<----- ENTER PATH HERE
 
 
